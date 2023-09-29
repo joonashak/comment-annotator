68efdd4a9fbabcfce75f63d725f3887b35134c93
joonashak <joonashak@proton.me>
2023-09-29 16:31:11 +0300

    Use different types of annotations to kind of group common comments

--- annotate.sh
+++ annotate.sh
@@ -2,6 +2,19 @@
 
 grep_result=$(grep -RnE "^\s*(//|#|\!|'|;)\s*(TODO|FIXME).*")
 
+set_ann_type () {
+  if [[ $1 =~ "TODO" ]]
+  then
+    ANN_TYPE="warning"
+  elif [[ $1 =~ "FIXME" ]]
+  then
+    ANN_TYPE="error"
+  else
+    ANN_TYPE="notice"
+  fi
+}
+
 while IFS=':' read -r path line message; do
-  echo "::warning file=${path},line=${line}::${message}"
+  set_ann_type "$message"
+  echo "::${ANN_TYPE} file=${path},line=${line}::${message}"
 done <<< "$grep_result"
