37e5abdaf89693f5d1e08b89879dd2fbfc27587d
joonashak <joonashak@proton.me>
2023-09-29 19:32:33 +0300

    Make grep include param configurable

--- annotate.sh
+++ annotate.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Default regex pattern can be overridden with $PATTERN env var.
 default_pattern="^\s*(//|#|\!|'|;)\s*(TODO|FIXME).*"
 pattern=${PATTERN:-$default_pattern}
 
-grep_result=$(grep -RnE --exclude-dir=.git "$pattern")
+# Parsed files can be limited with glob pattern in $INCLUDE env var.
+include=${INCLUDE:-"*"}
 
+grep_result=$(grep -RnE --exclude-dir=.git --include="$include" "$pattern")
+
+# Group TODO and FIXME-like annotations by using different annotation severity.
 set_ann_type () {
   if [[ $1 =~ "TODO" ]]
   then
@@ -17,7 +22,13 @@ set_ann_type () {
   fi
 }
 
+# Iterate over grep results, printing annotation strings.
 while IFS=':' read -r path line message; do
+  # Grep might return an empty result. Skip those.
+  if [[ -z $message ]]; then
+    continue
+  fi
+
   set_ann_type "$message"
   echo "::${ANN_TYPE} file=${path},line=${line}::${message}"
 done <<< "$grep_result"
